5f153a2db21eab1a3f3fc800fa908cae26128e5d
Arvind Srinivasan <arvind.srinivasan@gmail.com>
2012-09-28 16:46:01 -0400

    group rare, add viterbi-incomplete

--- assn/a1/code/ne_tagger.py
+++ assn/a1/code/ne_tagger.py
@@ -8,6 +8,7 @@ import optparse
 import shutil
 from collections import defaultdict
 from count_freqs import Hmm
+
 """
 random assertions that are verified as correct to avoid dumb mistakes
 """
@@ -17,10 +18,11 @@ def test_minimal():
 	t_rare = Tagger('ner_train-rare.dat')
 	assert t_rare.compute_emission('University','I-ORG'), 0.00130039011704
 
+
 """
 takes argmax of tag probabilities using the model specfied in Question 4 and writes results to q4_prediction_file and q4_prediction_file-rare.
 """
-def write_tag_probabilities(rare=True):
+def write_tag_probabilities(rare=True,num_lines=60000):
 	t = Tagger("ner_train-rare.dat") if rare else Tagger("ner_train.dat")
 	outfile = "q4_prediction_file-rare" if rare else "q4_prediction_file"
 	with open(outfile, "w") as f1, open("ner_dev.dat") as f:
@@ -29,7 +31,7 @@ def write_tag_probabilities(rare=True):
 				p = t.tag_probabilities(line.strip())
 				max_prob = max(p, key=p.get)
 				# can't take log of 0, so defaults
-				f1.write("%s %s %s\n" % (line.strip(), max_prob, (math.log(p[max_prob])) if p[max_prob] != 0 else 0))
+				f1.write("%s %s %s\n" % (line.strip(), max_prob, ln(p[max_prob])))
 			else:
 				f1.write(line)
 
@@ -39,14 +41,14 @@ def interactive_log_prob():
 		try:
 			trigram = raw_input("\n Enter the trigram - yi-2, yi-1, yi separated by spaces: ").split(' ')
 			trigram.reverse()
-			print "Log Probability: %s" % (math.log(t.compute_trigram(*trigram)) if t.compute_trigram(*trigram) > 0 else 0)
+			print "Log Probability: %s" % ln(t.compute_trigram(*trigram))
 		except EOFError:
 			break
 
 def test_viterbi(rare=False):
 	t = Tagger("ner_train-rare.dat") if rare else Tagger("ner_train.dat")
 	outfile = "viterbi_prediction_file-rare" if rare else "viterbi_prediction_file"
-	t.viterbi('ner_dev.dat',outfile)
+	t.viterbi('ner_dev.dat',outfile,num_lines=60000)
 
 """
 The core tagger, implements the functionality for q4-q6. Imports functionality from count_freqs directly 
@@ -60,12 +62,20 @@ class Tagger(object):
 		self.unigrams = {k[0]:v for k,v in self.counter.ngram_counts[0].iteritems()}
 		self.bigrams = self.counter.ngram_counts[1]
 		self.trigrams = self.counter.ngram_counts[2]
-
+	"""
+	conditional probability that the word maps to tag given the number of times the tag occurs
+	"""
 	def compute_emission(self, word, tag):
 		em = self.counter.emission_counts
-		return em.get((word,tag), em.get(('_RARE_',tag), 0)) / float(self.unigrams[tag])
+		if tag == '*':
+			return 0
+		if (word,tag) in em:
+			return em[(word,tag)]/float(self.unigrams[tag])
+		else:
+			# print "%s : %s" % (('_RARE_',tag),em[('_RARE_',tag)])
+			return em[('_RARE_',tag)]/float(self.unigrams[tag])
 
-	def compute_trigram(self,yi,y1='*',y2='*'):
+	def compute_trigram(self,yi,y1,y2):
 		return float(self.trigrams.get((y2,y1,yi),0))/self.bigrams.get((y2,y1),1)
 
 	"""
@@ -73,19 +83,25 @@ class Tagger(object):
 	is considered "rare"
 	"""
 	def replace_rare(self,infile,threshold=5):
-		common_words = [k[0] for k,v in self.counter.emission_counts.iteritems() if v >= threshold]
+		wordcounts = defaultdict(int)
+		for tup in self.counter.emission_counts.iteritems():
+			wordcounts[tup[0][0]] += tup[1] #aggregates counts of words total, with any tag
+		common_words = [k for k,v in wordcounts.iteritems() if v >= threshold]
+		replaced = 0
 		f = open(infile)
 		f2 = open(infile.replace('.dat','-rare.dat'), 'w')
 		for line in f:
 			if len(line.split(' ')) == 2:
 				if line.split(' ')[0] not in common_words: #closed set, there are more rare than not rare, we know it's one or the other
 					f2.write(line.replace(line.split(' ')[0], '_RARE_', 1))
+					replaced +=1
 				else:
 					f2.write(line)
 			else:
 				f2.write(line) # probably not necessary to write empty lines, but useful to check that it didn't lose data
 		f.close()
 		f2.close()
+		print replaced
 	"""
 	returns a dictionary of relative probabilities for emission counts
 	"""
@@ -94,26 +110,39 @@ class Tagger(object):
 		prob = lambda v: v/sum(counts.values()) if sum(counts.values()) != 0 else 0
 		return {k:prob(v) for k,v in counts.iteritems()}
 
-	def viterbi(self,infile,outfile):
+	def viterbi(self,infile,outfile,num_lines):
 		with open(infile) as f, open(outfile,"w") as f2:
 			possible_tags = self.unigrams.keys()
-			tags = ['*','*']
-			mat = defaultdict(int)
+			possible_tags.append('*')
+			mat = defaultdict(lambda: 1)
 			mat [(0,'*','*')] = 1
-			w = '*'
-			for idx,line in enumerate(f):
-				word = line.strip() if line!='\n' else 'S_STOP_S'
-				v,u = tags[-1:][0], tags[-2:-1][0]
-				pi_n = {w:mat[(idx,w,u)]*self.compute_trigram(v,w,u)*self.compute_emission(word,w) for w in possible_tags}
-				print pi_n
-				tag = max(pi_n, key=pi_n.get)
-				cur_pi = pi_n[tag]
-				tags.append(tag)
-				print (idx+1,u,v)
-				mat[(idx+1,u,v)] = cur_pi
-				f2.write('%s %s %s' % (word,tag,cur_pi))
+			idx = 0
+			for line in f:
+				if line == '\n':
+					f2.write('\n')
+					mat = defaultdict(lambda: 1)
+					mat [(0,'*','*')] = 1
+					idx = 0
+					continue
+				word = line.strip()
+				tags = []
+				for u in possible_tags:
+					for v in possible_tags:
+						pi_n = {w:mat.get((idx,w,u),1)*self.compute_trigram(v,w,u)*self.compute_emission(word,w) for w in possible_tags}
+						tag = max(pi_n, key=pi_n.get)
+						cur_pi = pi_n[tag]
+						mat[(idx+1,u,v)] = cur_pi
+						tags.append((tag,cur_pi))
+				# print tags
+				argmax = max(tags, key=lambda x: x[1])
+				f2.write('%s %s %s\n' % (word,argmax[0], ln(argmax[1])))
+				idx+=1
+				num_lines -= 1
+				if num_lines == 0:
+					return
 
 if __name__ == "__main__":
+	ln = lambda x: math.log(x) if x > 0 else 0
 	t = Tagger()
 	parser = optparse.OptionParser()
 	parser.add_option("-i", "--interactive",
@@ -123,7 +152,8 @@ if __name__ == "__main__":
 	if options.interactive:
 		interactive_log_prob()
 	else:
-		# t.replace_rare('ner_train.dat',10)
+		t.replace_rare('ner_train.dat',5)
 		# write_tag_probabilities(True)
 		# write_tag_probabilities(False)
-		test_viterbi()
+		# test_viterbi(True)
+		# test_viterbi(False)
